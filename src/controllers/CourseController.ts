5a77ea13607bdf14bfe2671ba12cbec9890bc5be
Syam Iyer <iyersyam@gmail.com>
2020-12-15 22:10:43 +1100

    Finished the CourseController

--- src/controllers/CourseController.ts
+++ src/controllers/CourseController.ts
@@ -1,12 +1,42 @@
 import * as express from 'express';
+import * as _ from 'lodash';
+const Fuse = require('fuse.js');
 const router = express.Router();
 
-router.get('/get', function (request: express.Request, response: express.Response) {
-    response.send(`Course Request received: ${request.method} - ${request.path} with TS`);
+const courses = require('../../data_v2/courses.json');
+import { Course } from '../types';
+
+interface ReturnedCourse {
+    title: Course
+}
+
+function getCourse(course_code: string): ReturnedCourse {
+    const course: ReturnedCourse = _.find(courses, function (spec) {
+        return spec.Item.course_code === course_code;
+    });
+
+    return course;
+}
+
+router.post('/get', function (request: express.Request, response: express.Response) {
+    const course_code: string = request.body.course_code;
+
+    const courseToReturn = getCourse(course_code);
+
+    return response.send(courseToReturn);
 });
 
 router.post('/autocomplete', function (request: express.Request, response: express.Response) {
-    response.send(`Course Request received: ${request.method} - ${request.path} with TS`);
+    const query = request.body.query;
+
+    const fuse = new Fuse(courses, {
+        keys: ['Item.course_code', 'Item.name']
+    });
+
+    const results = fuse.search(query, { limit: 20 });
+    const uniqueResults = _.uniqBy(results, 'item.Item.course_code');
+
+    return response.send(uniqueResults);
 });
 
 module.exports = router;
---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+interface Course {
+    course_code: string;
+    implementation_year: string;
+    link: string;
+    name: string;
+    credit_points: string;
+}
+
+interface Program {
+
+}
+
+interface Specialisation {
+
+}
+
+export {
+    Course, 
+    Program, 
+    Specialisation
+}
